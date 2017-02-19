c2f4b94efb6f2044f2f85dec9bd475c807e3e637
Christian Beikov <christian.beikov@gmail.com>
2017-02-19 08:39:30 +0100

    Fix build script

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-${MVN_BIN} -version
+mvn -version
 
 PROPERTIES="$PROPERTIES -Duser.country=US -Duser.language=en"
 export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=512m"
 
-eval exec ${MVN_BIN} -P ${JPAPROVIDER},${RDBMS} install -am $PROPERTIES
+eval exec mvn -P ${JPAPROVIDER},${RDBMS} install -am $PROPERTIES
